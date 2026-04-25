f0cc3e5c0063ecb2bf332e03cfb01a497b1ea17a
Minh2255 <80587574+trongminh13@users.noreply.github.com>
2026-04-25 16:47:52 +0700

    feat: create new python data and math

---
+++ PyThon_Basic/python_basic_1.py
@@ -0,0 +1,44 @@
+print("=====================================================")
+a =7
+b= 6.5
+c = 'today is Monday'
+d = "Ronaldo better than Messi"
+e = """ I can do is all day """
+f = True
+f = "Alo Vu a Vu"
+print(a)
+print(type(f))
+print(4+5)
+print(6-1.5)
+print(4*2)
+print()
+print(a!= 7.5)
+""""Em choi sao duoc"""
+'''Khong anh oi'''
+# ALO em khong biet anh a
+print("=====================================================")
+name = input("What is your name? ")
+print(name)
+so = input("What is your address? ")
+print(so)
+ep = int(so)
+print(type(ep))
+
+pi = 3.14
+radius = int(input("Please enter the radis of the circle: "))
+c= 2 * pi ** radius
+print("The circumference of the circle is",c)
+
+a = 5
+b = 6
+print(a,b)
+temp = a
+a = b
+b = temp
+print(a,b)
+
+print("=====================================================")
+
+a,b = 1,2
+a,b = b,a
+print(a,b)
---
+++ PyThon_Basic/python_basic_2.py
@@ -0,0 +1,32 @@
+
+a = 4
+if a > 3:
+    b = True
+
+a =4
+if a > 3:
+    print("start 0f block")
+    b = True
+    c = 5
+    print("End of block")
+
+print("Next Step")
+
+# Cau lenh pass
+a = 4
+if a > 3:
+    pass
+    'Dung khi de day khi nao dung thi dung , tap thoi pass'
+print("next")
+
+# match-case
+
+# toán tử 3 ngôi
+x = 5
+if x % 2 == 0:
+    number = "Even"
+else:
+    number = "Odd"
+
+x = 5
+number = "Even" if x % 2 == 0 else "Odd"
